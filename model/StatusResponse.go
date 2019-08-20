796463121ae95ae2c5b69e6b56aae09aa58d7b96
Dhairya Punjabi <dhairyapunjabi49@gmail.com>
2019-08-20 11:50:34 +0530

    Create status response struct

---
+++ model/StatusResponse.go
@@ -0,0 +1,14 @@
+package controller
+
+import (
+	"encoding/json"
+)
+
+type StatusResponse struct {
+	ID           string            `json:"id"`
+	StartTime    string            `json:"start_time"`
+	EndTime      string            `json:"end_time"`
+	Status       string            `json:"status"`
+	DownloadType string            `json:"download_type"`
+	Files        map[string]string `json:"files"`
+}
